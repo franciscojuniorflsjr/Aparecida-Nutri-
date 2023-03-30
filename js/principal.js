2e595a1afc1ad11a6f75454c7a2cbe1049e2b20c
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-30 18:18:23 -0300

    aula 02 Variáveis e Operadores

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,44 @@
 var titulo = document.querySelector("h1");
-			
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso")
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+ 
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+
+if(peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!")
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!"
+}
+if(altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!")
+    alturaEhValido = false;
+    tdImc.textContent = "Altura inválida!"
+}
+
+if( alturaEhValido && pesoEhValido) {
+    var imc = peso / (altura * altura); 
+    tdImc.textContent = imc;
+}
+
+
+/*
+var tdGordura = paciente.querySelector(".info-gordura")
+var gordura = tdGordura.textContent;
+
+console.log(paciente); // tr 
+console.log(tdPeso); // td que tem o peso
+console.log(peso); // Obter 100
+console.log(altura);
+console.log(gordura);
+*/
