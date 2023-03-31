4307bc9eb489de5eea3c101235558b6ad0da0a1e
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-31 17:49:06 -0300

    atualização

--- js/principal.js
+++ js/principal.js
@@ -1,44 +1,42 @@
 var titulo = document.querySelector("h1");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelector(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso")
-var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length ; i++) {
+
+    var paciente = pacientes[i];
+   console.log(paciente[i]);
+   
+    var tdPeso = paciente.querySelector(".info-peso")
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
  
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-var pesoEhValido = true;
-var alturaEhValido = true;
+    var pesoEhValido = true;
+    var alturaEhValido = true;
 
 
-if(peso <= 0 || peso >= 1000) {
+    if(peso <= 0 || peso >= 1000) {
     console.log("Peso inválido!")
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido!"
-}
-if(altura <= 0 || altura >= 3.00) {
+    }
+    if(altura <= 0 || altura >= 3.00) {
     console.log("Altura inválida!")
     alturaEhValido = false;
     tdImc.textContent = "Altura inválida!"
 }
 
-if( alturaEhValido && pesoEhValido) {
+    if( alturaEhValido && pesoEhValido) {
     var imc = peso / (altura * altura); 
     tdImc.textContent = imc;
 }
 
+}
 
-/*
-var tdGordura = paciente.querySelector(".info-gordura")
-var gordura = tdGordura.textContent;
 
-console.log(paciente); // tr 
-console.log(tdPeso); // td que tem o peso
-console.log(peso); // Obter 100
-console.log(altura);
-console.log(gordura);
-*/
