f0b2b6f5be72620f1497882a87183919cd592e11
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-04-01 18:29:58 -0300

    Aula utilizando  Arrays , Loop e Estilos

--- js/principal.js
+++ js/principal.js
@@ -1,13 +1,12 @@
 var titulo = document.querySelector("h1");
 titulo.textContent = "Aparecida Nutricionista";
 
-var pacientes = document.querySelector(".paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
 
 for (var i = 0; i < pacientes.length ; i++) {
 
     var paciente = pacientes[i];
-   console.log(paciente[i]);
    
     var tdPeso = paciente.querySelector(".info-peso")
     var peso = tdPeso.textContent;
@@ -17,26 +16,32 @@ for (var i = 0; i < pacientes.length ; i++) {
  
     var tdImc = paciente.querySelector(".info-imc");
 
+    var alturaEhValida = true;
     var pesoEhValido = true;
-    var alturaEhValido = true;
-
+    
 
     if(peso <= 0 || peso >= 1000) {
     console.log("Peso inválido!")
     pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!"
+    tdImc.textContent = "Peso inválido!";
+    paciente.classList.add("paciente-invalido")
     }
+
     if(altura <= 0 || altura >= 3.00) {
     console.log("Altura inválida!")
-    alturaEhValido = false;
-    tdImc.textContent = "Altura inválida!"
-}
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+    paciente.classList.add("paciente-invalido")
+    }
 
-    if( alturaEhValido && pesoEhValido) {
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / ( altura * altura); 
+        tdImc.textContent = imc;  
+    }
+    
+    if( alturaEhValida && pesoEhValido) {
     var imc = peso / (altura * altura); 
-    tdImc.textContent = imc;
-}
+    tdImc.textContent = imc.toFixed(2);  
+    }
 
 }
-
-
