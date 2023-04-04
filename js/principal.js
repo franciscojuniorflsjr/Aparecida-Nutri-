f6ae66efd1f98723c4f256ecb9e5a45bed3aeee1
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-04-04 11:17:28 -0300

    aula 04  Eventos, Formulários Criando Elementos

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
-var titulo = document.querySelector("h1");
+var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
@@ -45,3 +45,48 @@ for (var i = 0; i < pacientes.length ; i++) {
     }
 
 }
+
+    var botaoAdicionar = document.querySelector("#adicionar-paciente")
+    botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+    
+    var nome = form.nome.value
+    var peso = form.peso.value
+    var altura = form.altura.value
+    var gordura = form.gordura.value
+    
+   var pacienteTr = document.createElement("tr");
+
+   var nomeTd = document.createElement("td");
+   var pesoTd = document.createElement("td");
+   var alturaTd = document.createElement("td");
+   var gorduraTd = document.createElement("td");
+   var imcTd = document.createElement("td");
+   
+   nomeTd.textContent = nome;
+   pesoTd.textContent = peso;
+   alturaTd.textContent = altura;
+   gorduraTd.textContent = gordura;
+   imcTd.textContent = imc;
+
+   pacienteTr.appendChild(nomeTd);
+   pacienteTr.appendChild(pesoTd);
+   pacienteTr.appendChild(alturaTd);
+   pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+
+
+    });
+
+
+
+/*
+        titulo.addEventListener("click", function() {
+            console.log("Olá eu fui clicado agora com função anonima!")
+        });
+
+*/
